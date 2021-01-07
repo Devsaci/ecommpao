26d41b952cfe9161d1b9b9e9a57650ba29649205
saci zakaria <sacideveloppeur@gmail.com>
2021-01-07 20:51:24 +0100

    294 Implementation de la Persistance :  function createUser(UserEntity $user)

--- backend/model/DataLayer.class.php
+++ backend/model/DataLayer.class.php
@@ -21,4 +21,44 @@ class DataLayer
             echo $e->getMessage();
         }
     }
+
+/**
+     * Methode permettant de créer un utilisateur en BD 
+     * @param UserEntity $user Objet métier décrivant un un utilisateur
+     * @return TRUE Persistance réussie
+     * @return FALSE Echec de la persistance
+     * @return NULL Exception déclenchée
+     */
+    function createUser(UserEntity $user){
+        $sql = "INSERT INTO ".DB_NAME.".`customers` (sexe,pseudo,email,password,firstname,lastname,dateBirth)
+        VALUES (:sexe,:pseudo,:email,:password,:firstname,:lastname,:dateBirth)";try {
+           
+            $result = $this->connexion->prepare($sql); 
+
+            $data = $result->execute(array(
+                ':sexe' => $user->getSexe(),
+                ':pseudo' => $user->getPseudo(),
+                ':email' => $user->getEmail(),
+                ':password' => sha1($user->getPassword()),
+                ':firstname' => $user->getFirstname(),
+                'lastname' => $user->getLastname(),
+                ':dateBirth' => $user->getDateBirth()
+            ));
+
+            
+            if($data){
+                return TRUE;
+            }else {
+                return FALSE;
+            } 
+            
+        
+        } catch (PDOException $th) {
+            return NULL;
+        }
+
+
+    }
+
+
 }
