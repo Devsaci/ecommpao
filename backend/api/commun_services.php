e0ba29441e7dc0778efa4c637dffa4cf45977860
saci zakaria <sacideveloppeur@gmail.com>
2021-01-12 10:12:33 +0100

    definition des constantes

--- backend/api/commun_services.php
+++ backend/api/commun_services.php
@@ -1,4 +1,12 @@
 <?php 
 date_default_timezone_set("Europe/Paris");
 header("Content-type: application/json; charset=UTF-8");
+define("API", dirname(__FILE__));
+define("ROOT", dirname(API));
+define("SP",DIRECTORY_SEPARATOR);
+define("CONFIG", ROOT.SP."config"); 
+define("MODEL", ROOT.SP."model");
+define("ENTITY", ROOT.SP."entity");
+
+
 
