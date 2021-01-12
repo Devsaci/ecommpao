eb55e7e4a4b66b346be6a46183da0c9506b89b10
saci zakaria <sacideveloppeur@gmail.com>
2021-01-12 14:36:55 +0100

    => {   "status": 400,   "message": "Requête mal formulée",   "args": [
      ],
      "time": "12/01/2021 14:36:19"
    }

--- backend/api/authentifier.php
+++ backend/api/authentifier.php
@@ -1,9 +1,30 @@
 <?php
+session_start();
 
 require 'commun_services.php';
 
 
+// Cas où l'utilisateur est déjà connecté
+if(isset($_SESSION['ident'])){
+    produceError("utilisateur déjà connecté");
+    return;
+}
+
+
+// Cas où la requête est mal formulée
 if (!isset($_REQUEST['email']) || !isset($_REQUEST['password'])) {
     ProduceErrorRequest();
     return;
 }
+
+
+try {
+
+
+
+    
+    //code...
+} catch (Exception $th) {
+    produceError($th->getMessage());
+}
+
