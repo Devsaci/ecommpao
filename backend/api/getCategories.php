6abd5c0dd9aee5a7ea6d4b7c34f95db924356830
saci zakaria <sacideveloppeur@gmail.com>
2021-01-13 18:18:27 +0100

    Test produceResult(clearDataArray($categories))

--- backend/api/getCategories.php
+++ backend/api/getCategories.php
@@ -1,2 +1,12 @@
 <?php 
 require 'commun_services.php';
+try {
+    $categories = $db->getCategory();
+    if ($categories) {
+        produceResult(clearDataArray($categories));
+    }else {
+        produceError("Problème de Récupération des catégories");
+    }
+} catch (Exception $th) {
+    produceError($th->getMessage())
+}
