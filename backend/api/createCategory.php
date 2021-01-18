b5df4dd0d110e74b7716c38ca338ce111aede86b
saci zakaria <sacideveloppeur@gmail.com>
2021-01-18 17:41:20 +0100

    Test  $result

--- backend/api/createCategory.php
+++ backend/api/createCategory.php
@@ -11,10 +11,28 @@ if(!isset($_REQUEST['name']) ){
 
     $category = new CategoryEntity();
     $category->setName($_REQUEST['name']);
+ 
+
+
+    try {
+        $category = new CategoryEntity();
+        $category->setName($_REQUEST['name']);
+        
+        $result = $db->createCategory($category);
     
-    $result = $db->createCategory($category);
-
-
+    
+        if($result){
+            produceResult("Categorie créée avec succès");
+        }else{
+            produceError("Echec de création de la categorie");
+        }
+    
+    
+        
+    } catch (Exception $th) {
+        produceError($th->getMessage());
+    }
+    
 
 
 
