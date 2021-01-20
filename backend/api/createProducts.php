ed0e2929b010b1579e1ff9631e7c0069d6519814
saci zakaria <sacideveloppeur@gmail.com>
2021-01-20 11:54:05 +0100

    Not isset($_REQUEST['name']...

--- backend/api/createProducts.php
+++ backend/api/createProducts.php
@@ -1,2 +1,7 @@
 <?php 
 require 'commun_services.php';
+if(!isset($_REQUEST['name']) || !isset($_REQUEST['description']) || !isset($_REQUEST['price']) ||
+!isset($_REQUEST['stock']) || !isset($_REQUEST['category']) || !isset($_REQUEST['image'])){
+    produceErrorRequest();
+    return;
+}
