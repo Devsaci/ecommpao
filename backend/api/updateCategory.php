1da926fda46a1123d6ffea913098f2bf51ec6049
saci zakaria <sacideveloppeur@gmail.com>
2021-01-20 17:18:18 +0100

    Not isset/empty: id,name

--- backend/api/updateCategory.php
+++ backend/api/updateCategory.php
@@ -1,2 +1,12 @@
 <?php 
 require 'commun_services.php';
+
+if(!isset($_REQUEST['id']) || !isset($_REQUEST['name'])){
+    produceErrorRequest();
+    return;
+}
+
+if(empty($_REQUEST['id']) || empty($_REQUEST['name'])){
+    produceErrorRequest();
+    return;
+}
