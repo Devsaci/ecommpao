1009da914e7677dae4cafc8cb8f28d04ed752842
saci zakaria <sacideveloppeur@gmail.com>
2021-01-20 10:13:11 +0100

    Not isset idUser and  idProduct and  quantity and price

--- backend/api/createOrders.php
+++ backend/api/createOrders.php
@@ -1,2 +1,8 @@
 <?php 
 require 'commun_services.php';
+
+if(!isset($_REQUEST['idUser']) || !isset($_REQUEST['idProduct']) 
+|| !isset($_REQUEST['quantity']) || !isset($_REQUEST['price'])){
+    produceErrorRequest();
+    return;
+}
