2d0d31773086c37623e03e901efe5a1d7b548a8c
saci zakaria <sacideveloppeur@gmail.com>
2021-01-21 16:08:15 +0100

    Envoie image sur le serveur/move_uploaded_file()

--- backend/api/uploadImage.php
+++ backend/api/uploadImage.php
@@ -5,7 +5,7 @@ if(isset($_FILES) && is_array($_FILES)){
 
     if(isset($_FILES['image']["name"]) ){
         $dirImage = realpath("..")."/images/products/".$_FILES['image']["name"];
-
+        $save = move_uploaded_file($_FILES['image']["tmp_name"],$dirImage);
     }else{
 produceError("Fichier incorect");
     }
