36648d278c0c12c793c8813081b4e3235044a652
saci zakaria <sacideveloppeur@gmail.com>
2021-01-21 16:04:42 +0100

    Test : image ,name/Dir :images/products

--- backend/api/uploadImage.php
+++ backend/api/uploadImage.php
@@ -2,7 +2,13 @@
 require "commun_services.php";
 
 if(isset($_FILES) && is_array($_FILES)){
-   
+
+    if(isset($_FILES['image']["name"]) ){
+        $dirImage = realpath("..")."/images/products/";
+    }else{
+produceError("Fichier incorect");
+    }
+
 }else{
     produceErrorRequest();
 }
