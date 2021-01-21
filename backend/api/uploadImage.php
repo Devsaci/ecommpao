10117ccf170a6fe068ed95c54eb2d490bb921d0d
saci zakaria <sacideveloppeur@gmail.com>
2021-01-21 16:12:13 +0100

    Test $save

--- backend/api/uploadImage.php
+++ backend/api/uploadImage.php
@@ -6,6 +6,14 @@ if(isset($_FILES) && is_array($_FILES)){
     if(isset($_FILES['image']["name"]) ){
         $dirImage = realpath("..")."/images/products/".$_FILES['image']["name"];
         $save = move_uploaded_file($_FILES['image']["tmp_name"],$dirImage);
+
+        if($save){
+            produceResult($_FILLES);
+        }else{
+            produceError("Erreur de stockage de l'image");
+        }
+
+
     }else{
 produceError("Fichier incorect");
     }
