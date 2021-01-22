8189e18474dd696a84a7942f943c6b6cf6603ac5
saci zakaria <sacideveloppeur@gmail.com>
2021-01-22 14:49:18 +0100

    //DOCUMENTATION

--- backend/config/config.php
+++ backend/config/config.php
@@ -9,3 +9,15 @@
     define("DB_PASSWORD", "");
     define("HOST", "localhost");
     define("DB_NAME", "microbe_souck");
+
+
+   //DOCUMENTATION
+    $METHODES = [
+      "get"=>["description"=>"Lire les données","prefixe"=>"get" ],
+      "post"=>["description"=>"Créer une donnée","prefixe"=>"create" ],
+      "put"=>["description"=>"Mettre à jour une donnée","prefixe"=>"update" ],
+      "delete"=>["description"=>"Supprimer une donnée", "prefixe"=>"delete"],
+       
+    ];
+
+    $_ROUTES = ["products", "category", "orders","users"];
