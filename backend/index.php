ab26f7c61d48d996f0be0163f24cd73e13783d05
saci zakaria <sacideveloppeur@gmail.com>
2021-01-22 15:46:17 +0100

    DOCUMENTATION

--- backend/index.php
+++ backend/index.php
@@ -1,24 +1,106 @@
+
+<?php
+require 'config/config.php';
+define("BASE_URL", dirname($_SERVER['SCRIPT_NAME']));
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <style>
+   
+   a{
+     color: black;
+   }
+   p{
+    background-color: #fff; 
+    font-size: 35px;
+    border: 1px solid rgba(0,0,0,.1);
+    box-shadow:
+      0 10px 20px rgba(0,0,0,.22),
+      0 14px 56px rgba(0,0,0,.25);
+    
+   }
+   p span:nth-child(1){
+     display: inline-block;
+      font-size: 44px;
+      font-weight: 700;
+      min-width: 200px;
+      padding: 6px 15px;
+      text-align: center;
+      color: #fff;
+   }
+   p span:nth-child(2){
+    font-size: 35px;
+    font-weight: 700;
+   }
+   .get{
+    background-color: #3caab5;
+    text-transform: uppercase;
+   }
+   .post{
+    background-color: #78bc61;
+    text-transform: uppercase;
+   }
+   .delete{
+    background-color: #f93e3e;
+    text-transform: uppercase;
+   }
+   .put{
+    background-color: #fca130;
+    text-transform: uppercase;
+   }
+   nav.navbar{
+       background-color: #288690 !important;
+   }
+
+</style>
+
+    <title>API microbe_souck </title>
 </head>
 <body> 
-<h1><strong>EcommPao Init </strong></h1>
-    
+<!-- <h1><strong>DOCUMENTATION </strong></h1> -->
+<div class="container">
+<h1 class="text-center display-4">Documentation API microbe_souck</h1>
+
+<?php
+        
+        // Ouverture du dossier API
+        foreach ($_ROUTES as $key => $entity) {
+            $response = "<div id='$entity' class='display-4'><h4>".ucwords($entity)."</h4>";
+            foreach ($METHODES as $methode => $description) {
+              $response .= "<p><span class='$methode'> $methode </span> 
+              <span class='url'>
+              <a href='".BASE_URL."/api/$entity'target='_blank'> /api/$entity</a>
+              </span> 
+              ".$description['description']." : $entity</p>";
+            }
+            echo $response.'</div>';
+        }
+
+    ?>
+
+
+</div>
 </body>
 </html>
 
+
+
 <?php
 //import des parametres de connection à la BD 
-require 'config/config.php';
-require 'model/DataLayer.class.php';
-require 'entity/userEntity.php';
-require 'entity/categoryEntity.php';
-require 'entity/productEntity.php'; 
-require 'entity/ordersEntity.php'; 
+// require 'config/config.php';
+// require 'model/DataLayer.class.php';
+// require 'entity/userEntity.php';
+// require 'entity/categoryEntity.php';
+// require 'entity/productEntity.php'; 
+// require 'entity/ordersEntity.php'; 
 
 
 //Instantiation objet DataLayer
@@ -163,6 +245,5 @@ require 'entity/ordersEntity.php';
 
 
 
-?>
 
 
