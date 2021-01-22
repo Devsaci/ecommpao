c230b91b8d3ea1e6b282cea0a0bb76e82f0ef9c1
saci zakaria <sacideveloppeur@gmail.com>
2021-01-22 12:52:17 +0100

    => http://localhost/ecommpao/backend/api/products?id=10

--- backend/api/index.php
+++ backend/api/index.php
@@ -2,5 +2,13 @@
 // var_dump($_SERVER["REQUEST_URI"]);
 $url = trim($_SERVER["REQUEST_URI"],'/');
 $url_clean = explode("/", $url);
-var_dump($url_clean);
+// var_dump($url_clean);
 
+if(sizeof($url_clean) < 4){
+    header("Location: ../index.php");
+    exit();
+}else{
+    $action = $url_clean[sizeof($url_clean)-1];
+    
+}
+echo"test url ok";
