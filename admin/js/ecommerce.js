957ba85cd87f7a3195bf712e2105bb9a6770fe32
saci zakaria <sacideveloppeur@gmail.com>
2021-01-26 16:22:36 +0100

    Deletion

---
+++ admin/js/ecommerce.js
@@ -0,0 +1,29 @@
+class Ecommerce {
+
+    constructor() {
+        this.api_key = "API_KEY=adsffsdfds6b-6727-46f4-8bee-2c6ce6293e41";
+        this.api = "http://localhost/ecommerce/backend/api/";
+        this.actions = ['orders', 'users', 'category', 'products'];   
+    }
+
+    initRouter(){
+
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+export { Ecommerce }
