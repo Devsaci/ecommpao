5c05ae9e6941a8528dd1c4a98b5989be548901d2
saci zakaria <sacideveloppeur@gmail.com>
2021-01-28 15:37:53 +0100

    Connection .html et Router

--- admin/js/ecommerce.js
+++ admin/js/ecommerce.js
@@ -2,31 +2,31 @@ class Ecommerce {
 
     constructor() {
         this.api_key = "API_KEY=adsffsdfds6b-6727-46f4-8bee-2c6ce6293e41";
-        this.api = "http://localhost/ecommerce/backend/api/";
-        this.actions = ['orders', 'users', 'category', 'products'];  
+        this.api = "http://localhost/ecommpao/backend/api";
+        this.actions = [ "products","category", "orders","users"];
         this.initRouter();
     }
 
-    initRouter(){
+    initRouter() {
         this.actions.forEach((action) => {
-            document.getElementById(action).addEventListener('click', () => {
-                fetch('templates/' + action + '.html')
-                .then((response) => {
-                    if (response.ok) {
-                        return response.text();
-                    } else {
-                        console.log('Erreur de chargement du tempate');
-                    }
-                }).then((data) => {
-                    document.getElementsByClassName('container-fluid')[0].innerHTML = data;
+            document.getElementById(action).addEventListener("click", () => {
+                fetch("templates/" +action+ ".html")
+                    .then((response) => {
+                        if (response.ok) {
+                            return response.text();
+                        } else {
+                            console.log("Erreur de chargement du tempate");
+                        }
+                    }).then((data) => {
+                        document.getElementsByClassName("container-fluid")[0].innerHTML = data;
 
+                    })
+            })
         })
-    })
-})
-
     }
 
-}
-   
 
+}
 export { Ecommerce }
+
+
