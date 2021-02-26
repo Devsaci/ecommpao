62b7139dcd353b0bdceb45115f28f5054afb08b2
saci zakaria <sacideveloppeur@gmail.com>
2021-02-26 21:29:34 +0100

    Security function controlAccess() / $_REQUEST <=> $_POST

--- backend/api/createUsers.php
+++ backend/api/createUsers.php
@@ -2,25 +2,25 @@
 require 'commun_services.php';
 
 if (
-    !isset($_REQUEST["sexe"]) 
-    || !isset($_REQUEST["pseudo"])
-    || !isset($_REQUEST["email"])
-    || !isset($_REQUEST["password"]) 
-    || !isset($_REQUEST["firstname"]) 
-    || !isset($_REQUEST["lastname"])
-    || !isset($_REQUEST["dateBirth"])
+    !isset($_POST["sexe"]) 
+    || !isset($_POST["pseudo"])
+    || !isset($_POST["email"])
+    || !isset($_POST["password"]) 
+    || !isset($_POST["firstname"]) 
+    || !isset($_POST["lastname"])
+    || !isset($_POST["dateBirth"])
 ) {
     produceErrorRequest();
     return;
 }
 if (
-    empty($_REQUEST["sexe"]) 
-    || empty($_REQUEST["pseudo"])
-    || empty($_REQUEST["email"]) 
-    || empty($_REQUEST["password"])
-    || empty($_REQUEST["firstname"]) 
-    || empty($_REQUEST["lastname"])
-    || empty($_REQUEST["dateBirth"])
+    empty($_POST["sexe"]) 
+    || empty($_POST["pseudo"])
+    || empty($_POST["email"]) 
+    || empty($_POST["password"])
+    || empty($_POST["firstname"]) 
+    || empty($_POST["lastname"])
+    || empty($_POST["dateBirth"])
 ) {
     produceErrorRequest();
     return;
@@ -29,13 +29,13 @@ if (
 
 try {
     $user = new UserEntity();
-    $user->setSexe($_REQUEST["sexe"]);
-    $user->setPseudo(($_REQUEST["pseudo"]));
-    $user->setEmail($_REQUEST["email"]);
-    $user->setPassword($_REQUEST["password"]);
-    $user->setFirstname($_REQUEST["firstname"]);
-    $user->setLastname($_REQUEST["lastname"]);
-    $user->setDateBirth($_REQUEST["dateBirth"]);
+    $user->setSexe($_POST["sexe"]);
+    $user->setPseudo(($_POST["pseudo"]));
+    $user->setEmail($_POST["email"]);
+    $user->setPassword($_POST["password"]);
+    $user->setFirstname($_POST["firstname"]);
+    $user->setLastname($_POST["lastname"]);
+    $user->setDateBirth($_POST["dateBirth"]);
 
     $data = $db->createUser($user);
 
